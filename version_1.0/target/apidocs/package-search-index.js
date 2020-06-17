a02f04349b625da8b65a573e2b5867845b9a0c8b
Barbatos_Rex <1190387@isep.ipp.pt>
2020-06-17 04:31:13 +0100

    JavaDoc generated for both project versions

---
+++ version_1.0/target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"barbatos_rex1.io"}]
---
+++ version_1.0/target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"barbatos_rex1.io","l":"IOManager"},{"p":"barbatos_rex1.io","l":"Key"}]
